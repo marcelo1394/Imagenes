28fbc277d934b7557a194dc8cd73db376ebf56cd
Jefdaniel8 <jefrydaniel1998@gmail.com>
2025-05-20 22:46:11 -0500

    trabajoencasa

--- howwemade.php
+++ howwemade.php
@@ -64,6 +64,43 @@
     </iframe>
   </div>
 </section>
+  <div class="final">
+    <div class="imgfinal1">
+      <img width="180px" src="img/bombillo.png" alt="" />
+      <div class="form2">
+        <h1 class="text_form2">Subscribe now!</h1>
+        <form>
+          <div class="form2">
+            <input class="intform2" id="name" placeholder="Your Name" />
+          </div>
+          <div class="form2">
+            <input class="intform2" id="email" placeholder="Your Email" />
+          </div>
+          <div class="form2">
+            <input class="intform2" id="email" placeholder="Your Company" />
+          </div>
+        </form>
+        <button id="btnenviar" class="btnform2">Send</button>
+        <div class="container_follow">
+          <h4 class="follow">Follow us on:</h4>
+          <div class="follow_img">
+            <a
+              href="https://www.instagram.com/imagenes_graficas?igshid=di1ufcnwhc4f"><img width="40px" src="img/instagram.svg" alt="" /></a>
+            <a href="https://www.facebook.com/imagenesgraficasbicsas/"><img width="40px" src="img/facebook.svg" alt="" /></a>
+            <a
+              href="https://co.pinterest.com/igraficas/?invite_code=624950ea13b548a7b1c680f4787bd4c9&sender=506162583028394334"><img width="40px" src="img/pinterest.svg" alt="" /></a>
+          </div>
+        </div>
+      </div>
+      <div class="footer_img">
+        <img width="400px" src="img/9.png" alt="" />
+      </div>
+    </div>
+
+    <div class="texfinal">
+      Copyright 2025 Imagraphic - All Rights Reserved
+    </div>
+  </div>
 
 
 
